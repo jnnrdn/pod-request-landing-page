9729e23df47b1261c85a319debf34a7e5b21bb80
Jenny Ryden <jennyryden@protonmail.com>
2021-01-16 15:15:58 +0100

    Form validation

---
+++ src/js/script.js
@@ -0,0 +1,26 @@
+const form = document.getElementsByTagName('form')[0]
+
+const email = document.getElementById('email')
+const emailError = document.querySelector('#email + span.error')
+
+form.addEventListener('submit', function (e) {
+  if (email.validity.valid) {
+    emailError.textContent = '' // Reset the content of the message
+    emailError.className = 'error' // Reset the visual state of the message
+  } else {
+    showError()
+    e.preventDefault()
+  }
+})
+
+function showError() {
+  if (email.validity.valueMissing) {
+    console.log('Value missing! ', email.validity.valueMissing)
+    emailError.textContent = 'Oops! Please add your email'
+  } else if (email.validity.typeMismatch) {
+    console.log('Wrong format! ', email.validity.typeMismatch)
+    emailError.textContent = 'Oops! Please check your email'
+  }
+  // Set the styling appropriately
+  emailError.className = 'error active'
+}
